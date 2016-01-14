45659b0992a716c8888c86e45ca6baeba14b0556
Dmitriy Mamontov <mamontovdmitriy@mail.ru>
2016-01-14 21:30:27 +0600

    ads, share42

---
+++ protected/views/site/_ads_ga.php
@@ -0,0 +1,16 @@
+
+<!-- left ads block -->
+<div class="visible-lg" style="position: fixed; left: 0; top:0; bottom: 0; width: 250px;">
+    <?= Yii::$app->params['googleAds'] ?>
+</div>
+<div class="visible-md" style="position: fixed; left: 0; top:0; bottom: 0; width: 140px;">
+    <?= Yii::$app->params['googleAds'] ?>
+</div>
+
+<!-- right ads block -->
+<div class="visible-lg" style="position: fixed; right: 0; top:0; bottom: 0; width: 250px;">
+    <?= Yii::$app->params['googleAds'] ?>
+</div>
+<div class="visible-md" style="position: fixed; right: 0; top:0; bottom: 0; width: 140px;">
+    <?= Yii::$app->params['googleAds'] ?>
+</div>
---
+++ protected/views/site/_share42.php
@@ -0,0 +1,2 @@
+<div class="share42init pull-right" data-description="<?= $post->text ?>"></div>
+<script type="text/javascript" src="<?= Yii::$app->params['siteUrl'] ?>/share42/share42.js"></script>
