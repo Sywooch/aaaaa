07c5875b8087d6e2fffd77a6231b8721b9f027c7
Dmitriy Mamontov <mamontovdmitriy@mail.ru>
2016-05-14 14:09:29 +0600

    path

--- cron_deployer.php
+++ cron_deployer.php
@@ -6,5 +6,5 @@ $fileName = "/var/www/deploy/start_deploy.php";
 
 if (file_exists($fileName)) {
     unlink($fileName);
-    exec("cd /var/www/deploy & dep deploy > deploy_log.txt");
+    exec("cd /var/www/deploy && dep deploy > deploy_log.txt");
 }
